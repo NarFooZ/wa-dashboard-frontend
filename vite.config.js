8c7740f856fd3b6be780d6ec6b7d86de931f4364
NarFooZ <51388275+NarFooZ@users.noreply.github.com>
2026-06-11 17:36:24 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -8,5 +9,17 @@ export default defineConfig({
       '/api': 'http://localhost:3001',
       '/webhook': 'http://localhost:3001',
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // هذا الجزء يقوم بفصل مكتبات مثل Recharts و React في ملف منفصل (vendor)
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
